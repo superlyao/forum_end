ecaebcf3f9e7b5ce992b683e4883f82a652d6979
yliao <superyao12@sina.com>
2017-09-07 23:28:43 +0800

    update

--- src/main/java/com/yliao/web/TestController.java
+++ src/main/java/com/yliao/web/TestController.java
@@ -9,64 +9,70 @@ import org.springframework.util.StringUtils;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.yliao.service.CacheService;
+
 import java.util.List;
 
 
 /**
-* Created by superyao12@sina.com on 2017-09-06 22:10:20.
-*/
+ * Created by superyao12@sina.com on 2017-09-06 22:10:20.
+ */
 @Controller
 @RequestMapping("/test")
 public class TestController {
 
-@Autowired
-CacheService cacheService;
+    @Autowired
+    CacheService cacheService;
 
-@Autowired
-BookDAO bookDAO;
-/**
-* 测试数据库是否正常
-* @return
-*/
-@RequestMapping("/database")
-public String testDatabase(Model model) {
-StringBuilder builder = new StringBuilder();
-List<Book> list = bookDAO.getAllBooks();
-    for (Book book :
-    list) {
-    builder.append(book.toString()).append("\n");
-    }
-    if(StringUtils.isEmpty(builder.toString())){
-    model.addAttribute("result","请先往数据库添加测试数据------");
-    }
-    model.addAttribute("result",builder.toString());
-    return "/testDatabase";
+    @Autowired
+    BookDAO bookDAO;
+
+    /**
+     * 测试数据库是否正常
+     *
+     * @return
+     */
+    @RequestMapping("/database")
+    public String testDatabase(Model model) {
+        StringBuilder builder = new StringBuilder();
+        List<Book> list = bookDAO.getAllBooks();
+        for (Book book :
+                list) {
+            builder.append(book.toString()).append("\n");
+        }
+        if (StringUtils.isEmpty(builder.toString())) {
+            model.addAttribute("result", "请先往数据库添加测试数据------");
+        }
+        model.addAttribute("result", builder.toString());
+        return "/testDatabase";
     }
 
     /**
-    * 测试页面跳转是否正常
-    * @param model
-    * @return
-    */
-@RequestMapping("/index")
-public String testPage(Model model){
-System.out.println("success!!!!");
-model.addAttribute("result","project is running successfully!!!");
-return "/index";
-}
+     * 测试页面跳转是否正常
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping("/index")
+    public String testPage(Model model) {
+        System.out.println("success!!!!");
+        model.addAttribute("result", "project is running successfully!!!");
+        return "/index";
+    }
 
     /**
-    * 测试cache配置是否成功
-    * @return
-    */
+     * 测试cache配置是否成功
+     *
+     * @return
+     */
     @RequestMapping("/cache")
     public String testCache() {
-    String value = cacheService.testCache("cacheTest");
-    return "/cacheTest";
+        String value = cacheService.testCache("cacheTest");
+        return "/cacheTest";
     }
+
     @RequestMapping("/nositemesh")
     public String noSitemesh(Model model) {
-    model.addAttribute("result", "这是不带有sitemesh的页面");
-    return "/nositemesh";
+        model.addAttribute("result", "这是不带有sitemesh的页面");
+        return "/nositemesh";
     }
 }
