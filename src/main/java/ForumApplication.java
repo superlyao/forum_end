36a96639dc22d30d3c060e68681c2e61350eb124
yliao <superyao12@sina.com>
2017-12-26 20:42:26 +0800

    init

---
+++ src/main/java/ForumApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @Author liaoyao
+ * @Date 2017/12/26.
+ */
+@EnableAutoConfiguration
+public class ForumApplication {
+    public static void main(String[] arg) {
+        SpringApplication.run(ForumApplication.class, arg);
+    }
+}
